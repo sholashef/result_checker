adf9824c2ab4c4188f90a0664f40cf88031a7fe0
Oluwasola <113778568+sholashef@users.noreply.github.com>
2023-09-19 18:49:37 +0100

    Add files via upload

---
+++ result_checker.py
@@ -0,0 +1,22 @@
+name = input("Please Enter your name")#To receive name input
+student_name = name.upper() # convert entered name to upper case
+#student_register = (ROYHAN,IMRAN,AYMAN,SAFIYAH,SOLA,NAFI)
+if student_name == "ROYHAN": #conditional statement
+    print("Your score is: 98")
+elif student_name == "IMRAN":
+    print("Your Score is: 91")
+elif student_name == "AYMAN":
+    print("Your Score is: 82")
+elif student_name == "SAFIYAH":
+    print("Your Score is: 95")
+elif student_name == "SOLA":
+    print("Your Score is: 83")
+elif student_name == "NAFI":
+    print("Your Score is: 99")
+else:
+    print ("No Match Found!")#if entered name is not in the list print msg 
+    
+    
+
+
+
